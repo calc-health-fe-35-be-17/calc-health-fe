9f14c7222930b73a2d136dd43b2b684e36a4988d
perdanaph <danangputro9@gmail.com>
2023-06-01 18:26:32 +0700

    feat: create component card

---
+++ src/components/Card/CardFood.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unescaped-entities */
+import Card from 'react-bootstrap/Card';
+import ButtonPrimary from '../Button/ButtonPrimary/Button';
+// import { foodCard } from './../../fakers';
+import './card.css';
+export default function CardFood(props) {
+  const { img, title, calori, carbon } = props;
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Img variant="top" src={img} />
+        <Card.Title className="food-title">{title}</Card.Title>
+        <Card.Text>
+          <p className="calori">{calori} Kkal</p>
+          <p className="carbon">{carbon} KgCO2</p>
+        </Card.Text>
+        <div className="d-flex justify-content-between">
+          <ButtonPrimary style={'btn-'}>Pilih</ButtonPrimary>
+          <ButtonPrimary style={'btn-'}>Detail</ButtonPrimary>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
---
+++ src/fakers/index.js
@@ -0,0 +1,34 @@
+export const foodCard = [
+  {
+    img: 'https://images.unsplash.com/photo-1536688261748-9d56b3acf077?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
+    title: 'Cake',
+    nutrition: {
+      kalori: 2.11,
+      carbon: 0.34,
+    },
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1644288232801-2b40ff99d54f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1854&q=80',
+    title: 'Sate Ayam',
+    nutrition: {
+      kalori: 2.11,
+      carbon: 0.34,
+    },
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1596956470007-2bf6095e7e16?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=429&q=80',
+    title: 'Hamburger',
+    nutrition: {
+      kalori: 2.11,
+      carbon: 0.34,
+    },
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1532452960476-f234eee8bd71?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80',
+    title: 'Roti Bakar',
+    nutrition: {
+      kalori: 2.11,
+      carbon: 0.34,
+    },
+  },
+];
