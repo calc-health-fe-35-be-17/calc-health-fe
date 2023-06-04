16f0cdf6a0ddf005fe4fc53ef7aad17b8bc1ca77
AnggunKurniatul <aggnkurnkurn15@gmail.com>
2023-06-04 21:00:26 +0700

    Create TrackDiet page

---
+++ src/pages/TrackDiet.jsx
@@ -0,0 +1,26 @@
+import { Form } from "react-bootstrap";
+import ButtonPrimary from "../components/Button/ButtonPrimary/Button";
+import './TrackDiet.css'
+
+function TrackDiet(){
+    return(
+        <>
+            <div id="diet">
+                <h1>Kalkulator Berat Badan</h1>
+                <div id="calculator">
+                    <Form>
+                        <Form.Label className="label">Berat Badan</Form.Label>
+                        <Form.Control className='input' type='number' placeholder='Berat Badan (KG)'/>
+                        <Form.Label className="label">Tinggi Badan</Form.Label>
+                        <Form.Control className='input' type='number' placeholder='Tinggi Badan (CM)'/>
+                        <div id="hitung">
+                            <ButtonPrimary style="hitung" >HITUNG</ButtonPrimary>
+                        </div>
+                    </Form>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default TrackDiet
