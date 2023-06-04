5410179f7156f7abd3b9897d5ff4d51a09fdb6dd
perdanaph <danangputro9@gmail.com>
2023-06-05 01:48:32 +0700

    feat: add routing and fixing some component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,23 @@
-import './App.css';
-import Foods from './pages/foods';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './layouts/layout';
+import LandingPage from './pages/landing-page/landing-page';
+import Foods from './pages/foods/foods';
 import TrackDiet from './pages/track-diet/track-diet';
-// import LandingPage from './pages/landing-page/landing-page';
-// import { Col, Container, Row } from 'react-bootstrap';
-// import ButtonPrimary from './components/Button/ButtonPrimary/Button';
-// import ButtonSecond from './components/Button/ButtonSecond/button';
-// import CardFood from './components/Card/CardFood';
-// import { foodCard } from './fakers/index';
-// import NavigationBar from './components/Navbar/Navbar';
-// import FooterComponent from './components/Footer/footer';
-// import Hero from './components/Hero/Hero';
-// import ContentLandingPage from './components/Content/ContentLandingPage';
-// import LandingPage from './pages/LandingPage';
+import Login from './pages/login';
+import Register from './pages/register';
 
 function App() {
   return (
     <>
-      {/* <LandingPage /> */}
-      <TrackDiet />
-
-      {/* <Container>
-        <Row>
-          {foodCard.map((food, index) => (
-            <Col xs={12} sm={6} md={6} lg={4} key={index}>
-              <CardFood
-                img={food.img}
-                title={food.title}
-                calori={food.nutrition.kalori}
-                carbon={food.nutrition.carbon}
-              />
-            </Col>
-          ))}
-        </Row>
-      </Container> */}
+      <Layout>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/makanan" element={<Foods />} />
+          <Route path="/trackdiet" element={<TrackDiet />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Layout>
     </>
   );
 }
--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -2,7 +2,7 @@ import { Spinner } from 'react-bootstrap';
 
 const Loading = () => {
   return (
-    <div className="d-flex justify-content-center">
+    <div className="d-flex justify-content-center my-5">
       <Spinner animation="border" variant="success" />
     </div>
   );
