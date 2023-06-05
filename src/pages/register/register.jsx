4d80959aa358a84d9e3691e44c8306e826f892c9
AnggunKurniatul <aggnkurnkurn15@gmail.com>
2023-06-05 18:25:41 +0700

    Edit register and track-diet pages

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,49 +1,80 @@
-import Form from 'react-bootstrap/Form';
-import './register.css'
+import './register.css';
 import ButtonPrimary from '../../components/button/button-primary';
+import { Container, Form, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-function Register(){
-    return(
-        <>
-            <h1>Welcome to Calc Health</h1>
-            <div id='form'>
-                <Form>
-                    <Form.Group>
-                        <Form.Label className="mb-3" controlId="namadepan" >Nama Depan</Form.Label><br/>
-                        <Form.Control className='input' type='text' placeholder=''/>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label className="mb-3" controlId="namabelakang">Nama Belakang</Form.Label><br/>
-                        <Form.Control className='input' type='text' placeholder='' />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="email">
-                        <Form.Label>Email</Form.Label><br/>
-                        <Form.Control className='input' type="email" placeholder="" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="password">
-                        <Form.Label>Password</Form.Label><br/>
-                        <Form.Control className='input' type="password" placeholder="" />
-                    </Form.Group>
-                    <Form.Text id="passwordHelpBlock" muted>
-                        Minimal 8 karakter
-                    </Form.Text>
-                    <Form.Group className="mb-3" controlId="password">
-                        <Form.Label>Konfirmasi Password</Form.Label><br/>
-                        <Form.Control className='input' type="password" placeholder="" />
-                    </Form.Group>
-                    <Form.Group className="check" controlId="formBasicCheckbox">
-                        <Form.Check type="radio" id='check' label="Saya setuju dengan Syarat & Kententuan dan Kebijakan Privasi" />
-                    </Form.Group>
-                    <div className='buttondaftar'>
-                        <ButtonPrimary style="daftar">DAFTAR</ButtonPrimary>
-                    </div>
-                    <div className='buttondaftar' id='login'>
-                        <Form.Text>Sudah punya akun? <a href=''>Login</a> </Form.Text>
-                    </div>
-                </Form>
-            </div>
-        </>
-    )
+function Register() {
+  return (
+    <>
+      <Container className="my-5">
+        <Row className="flex-column justify-content-center align-items-center gap-lg-5">
+          <h1 className="title-register text-center">Welcome to Calc Health</h1>
+          <div className="form-container rounded">
+            <Form>
+              <Form.Group className="mb-4">
+                <Form.Label className="label">Nama Depan</Form.Label>
+                <Form.Control
+                  className="form-input input"
+                  type="text"
+                  placeholder=""
+                />
+              </Form.Group>
+              <Form.Group className="mb-4">
+                <Form.Label className="label">Nama Belakang</Form.Label>
+                <Form.Control
+                  className="form-input input"
+                  type="text"
+                  placeholder=""
+                />
+              </Form.Group>
+              <Form.Group className="mb-4">
+                <Form.Label className="label">Email</Form.Label>
+                <Form.Control
+                  className="form-input input"
+                  type="email"
+                  placeholder=""
+                />
+              </Form.Group>
+              <Form.Group className="mb-4">
+                <Form.Label className="label">Password</Form.Label>
+                <Form.Control
+                  className="form-input input"
+                  type="password"
+                  placeholder=""
+                />
+                <Form.Text className="mb-4" muted>
+                  Minimal 8 karakter
+                </Form.Text>
+              </Form.Group>
+              <Form.Group className="mb-4">
+                <Form.Label className="label">Konfirmasi Password</Form.Label>
+                <Form.Control
+                  className="form-input input"
+                  type="password"
+                  placeholder=""
+                />
+              </Form.Group>
+              <Form.Group className="w-75">
+                <Form.Check
+                  className="form-check check"
+                  type="radio"
+                  label="Saya setuju dengan Syarat & Kententuan dan Kebijakan Privasi"
+                />
+              </Form.Group>
+              <div className="daftar d-flex justify-content-center align-items-center my-4">
+                <ButtonPrimary style="daftar w-50 py-2">DAFTAR</ButtonPrimary>
+              </div>
+              <div className="buttonlogin text-end">
+                <Form.Text>
+                  Sudah punya akun? <Link to="/login">Login</Link>
+                </Form.Text>
+              </div>
+            </Form>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default Register
+export default Register;
--- src/pages/track-diet/track-diet.jsx
+++ src/pages/track-diet/track-diet.jsx
@@ -1,26 +1,42 @@
-import { Form } from "react-bootstrap";
-import ButtonPrimary from "../../components/button/button-primary";
-import './track-diet.css'
+import { Container, Form, Row } from 'react-bootstrap';
+import ButtonPrimary from '../../components/button/button-primary';
+import './track-diet.css';
 
-function TrackDiet(){
-    return(
-        <>
-            <div id="diet">
-                <h1>Kalkulator Berat Badan</h1>
-                <div id="calculator">
-                    <Form>
-                        <Form.Label className="label">Berat Badan</Form.Label>
-                        <Form.Control className='input' type='number' placeholder='Berat Badan (KG)'/>
-                        <Form.Label className="label">Tinggi Badan</Form.Label>
-                        <Form.Control className='input' type='number' placeholder='Tinggi Badan (CM)'/>
-                        <div id="hitung">
-                            <ButtonPrimary style="hitung" >HITUNG</ButtonPrimary>
-                        </div>
-                    </Form>
-                </div>
-            </div>
-        </>
-    )
+function TrackDiet() {
+  return (
+    <>
+      <Container className="my-5">
+        <Row className="flex-column justify-content-center align-items-center gap-lg-5">
+          <h1 className="title-diet text-center">Kalkulator Berat Badan</h1>
+          <div className="form-diet ">
+            <Form>
+              <Form.Group className="mb-4">
+                <Form.Label className="label">Berat Badan</Form.Label>
+                <Form.Control
+                  className="input-calc input"
+                  type="number"
+                  placeholder="Berat Badan (KG)"
+                />
+              </Form.Group>
+              <Form.Group className="mb-4">
+                <Form.Label className="label mt-3">Tinggi Badan</Form.Label>
+                <Form.Control
+                  className="input-calc input"
+                  type="number"
+                  placeholder="Tinggi Badan (CM)"
+                />
+              </Form.Group>
+              <div className="d-flex justify-content-center align-items-center">
+                <ButtonPrimary style="daftar w-50 py-2 mt-5">
+                  HITUNG
+                </ButtonPrimary>
+              </div>
+            </Form>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default TrackDiet
+export default TrackDiet;
