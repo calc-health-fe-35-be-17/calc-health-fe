c2686bf96ffc14531c201564ca5b6e061e80ac60
perdanaph <danangputro9@gmail.com>
2023-06-06 22:59:51 +0700

    feat: create pages track carbon and create nav-back in global component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import TrackDiet from './pages/track-diet/track-diet';
 import Login from './pages/login';
 import Register from './pages/register';
 import TrackCalori from './pages/track-calori';
+import TrackCarbon from './pages/track-carbon';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/makanan" element={<Foods />} />
         <Route path="/trackdiet" element={<TrackDiet />} />
         <Route path="/trackcalori" element={<TrackCalori />} />
+        <Route path="/trackcarbon" element={<TrackCarbon />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
