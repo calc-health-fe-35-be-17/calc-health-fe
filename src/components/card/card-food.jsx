d345867f0b8f19de4cc11c4b8beb9be7d56d0dd9
perdanaph <danangputro9@gmail.com>
2023-06-09 16:26:31 +0700

    fixing card

--- src/components/card/card-food.jsx
+++ src/components/card/card-food.jsx
@@ -7,13 +7,15 @@ import './card.css';
 export default function CardFood(props) {
   const { img, title, calori, carbon } = props;
   return (
-    <Card style={{ width: '14rem' }} className='rounded-0 border-0'>
-      <Card.Body className='p-0'>
-        <Card.Img variant="top" src={img}  className='rounded-0'/>
-        <Card.Title className="food-title fs-6 mt-2" style={{height: '55px'}}>{title}</Card.Title>
-        <Card.Text className='mb-0 px-2'>
-          <p className="calori">Calori: {calori}Kkal</p>
-          <p className="carbon">Carbon: {carbon}KgCO2</p>
+    <Card style={{ width: '14rem' }} className="rounded-0 border-0">
+      <Card.Body className="p-0">
+        <Card.Img variant="top" src={img} className="rounded-0" />
+        <Card.Title className="food-title fs-6 mt-2" style={{ height: '55px' }}>
+          {title}
+        </Card.Title>
+        <Card.Text className="mb-0 px-2 calori">Calori: {calori}Kkal</Card.Text>
+        <Card.Text className="mb-0 px-2 carbon">
+          Carbon: {carbon}KgCO2
         </Card.Text>
         <div className="d-flex justify-content-between pb-3 px-3">
           <ButtonPrimary style={'btn-card'}>Pilih</ButtonPrimary>
