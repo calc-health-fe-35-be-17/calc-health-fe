95b9be555860a6714e2794d0b7ca4fd2826ea8f6
perdanaph <danangputro9@gmail.com>
2023-06-16 10:48:57 +0700

    fixed generate alori

--- src/pages/track-calori/track-calori.jsx
+++ src/pages/track-calori/track-calori.jsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from 'react';
 import NavBack from './component/nav-back/nav-back';
 import DiagramBox from './partials/diagram-box/diagram-box';
@@ -7,44 +6,44 @@ import FormGenerate from './partials/form-generate/form-generate';
 import './track-calori.css';
 import { useSelector } from 'react-redux';
 
-
 export default function TrackCalori() {
-    const [caloriesNeed, setCaloriesNeed] = useState(0);
-    const cartFood = useSelector(state => state.cartFood);
+  const [caloriesNeed, setCaloriesNeed] = useState(0);
+  const cartFood = useSelector(state => state.cartFood);
 
-    const caloriesAmount = () => {
-        const result = cartFood.data.map((value) => value.nutrition.nutrients[0].amount).reduce((a, b) => a + b, 0);
-        return result;
+  const caloriesAmount = () => {
+    const result = cartFood.data
+      .map(value => value.nutrition.nutrients[0].amount)
+      .reduce((a, b) => a + b, 0);
+    return result;
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('bmr')) {
+      setCaloriesNeed(() => Number(localStorage.getItem('bmr')));
     }
+  }, [caloriesNeed]);
 
-
-    useEffect(() => {
-        if (localStorage.getItem('bmr')) {
-            setCaloriesNeed(() => Number(localStorage.getItem('bmr')))
-        }
-    }, [caloriesNeed])
-
-    const trackBMR = (gender, weight, height, age) => {
-        if (gender == 'Pria') {
-            return 66.5 + (13.7 * weight) + (5 * height) - (6.8 * age)
-        }
-        return 65.5 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
+  const trackBMR = (gender, weight, height, age) => {
+    if (gender == 'Pria') {
+      return 665 + 13.7 * weight + 5 * height - 6.8 * age;
     }
+    return 655 + 9.6 * weight + 1.8 * height - 4.7 * age;
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const { age, height, weight, gender } = e.target;
-        const bmr = trackBMR(gender.value, weight.value, height.value, age.value);
-        localStorage.setItem('bmr', bmr)
-        setCaloriesNeed(() => bmr);
-    }
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { age, height, weight, gender } = e.target;
+    const bmr = trackBMR(gender.value, weight.value, height.value, age.value);
+    localStorage.setItem('bmr', bmr);
+    setCaloriesNeed(() => bmr.toFixed(2));
+  };
 
-    return (
-        <div className='track-calori__wrapper'>
-            <NavBack />
-            <FormGenerate handleSubmit={handleSubmit} caloriesNeed={caloriesNeed} />
-            <DiagramBox caloriesNeed={caloriesNeed} caloriesAmount={caloriesAmount} />
-            <FoodList />
-        </div>
-    )
+  return (
+    <div className="track-calori__wrapper">
+      <NavBack />
+      <FormGenerate handleSubmit={handleSubmit} caloriesNeed={caloriesNeed} />
+      <DiagramBox caloriesNeed={caloriesNeed} caloriesAmount={caloriesAmount} />
+      <FoodList />
+    </div>
+  );
 }
