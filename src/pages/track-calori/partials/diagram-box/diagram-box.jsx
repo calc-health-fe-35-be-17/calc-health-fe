f49696ef3b129e4ba3c49c1fbf93b8a8c3d05101
perdanaph <danangputro9@gmail.com>
2023-06-16 19:03:35 +0700

    fixing some responsive design

--- src/pages/track-calori/partials/diagram-box/diagram-box.jsx
+++ src/pages/track-calori/partials/diagram-box/diagram-box.jsx
@@ -1,37 +1,17 @@
 import { Col, Row } from 'react-bootstrap';
 import './diagram-box.css';
-// import CompositionDiagram from '../../component/composition-diagram/composition-diagram';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 export default function DiagramBox({ caloriesNeed, caloriesAmount }) {
-  // const composition = [
-  //     {
-  //         name: 'Karbohidrat',
-  //         count: 0,
-  //     },
-  //     {
-  //         name: 'Protein',
-  //         count: 0,
-  //     },
-  //     {
-  //         name: 'Lemak',
-  //         count: 0,
-  //     },
-  //     {
-  //         name: 'Karbondioksida',
-  //         count: 0,
-  //     }
-  // ]
-
   return (
     <div className="diagram-box__wrapper">
       <Row className="justify-content-between">
         <Col md={12}>
           <span className="required-calori_title">Kebutuhan Kalori Harian</span>
           <Row className="justify-content-between">
-            <Col md={7}>
-              <div className="circle-diagram">
+            <Col md={7} sm={12}>
+              <div className="circle-diagram my-3">
                 <CircularProgressbar
                   minValue={0}
                   maxValue={caloriesNeed}
@@ -46,7 +26,7 @@ export default function DiagramBox({ caloriesNeed, caloriesAmount }) {
               </div>
             </Col>
             <Col md={3}>
-              <ul className="calculate-calori">
+              <ul className="calculate-calori p-0">
                 <li>
                   <div className="d-flex align-items-center gap-3">
                     <div className="dibutuhkan"></div>
@@ -69,9 +49,6 @@ export default function DiagramBox({ caloriesNeed, caloriesAmount }) {
             </Col>
           </Row>
         </Col>
-        {/* <Col md={3}>
-                    {composition.map((item, index) => <CompositionDiagram key={index} name={item.name} count={item.count} />)}
-                </Col> */}
       </Row>
     </div>
   );
