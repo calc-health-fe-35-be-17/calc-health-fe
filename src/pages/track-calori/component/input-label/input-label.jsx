50e3b37f13e92a3743128c15e49cd4b89698f9bf
perdanaph <danangputro9@gmail.com>
2023-06-16 10:03:58 +0700

    feat: generate track calori

--- src/pages/track-calori/component/input-label/input-label.jsx
+++ src/pages/track-calori/component/input-label/input-label.jsx
@@ -1,17 +1,32 @@
 import './input-label.css';
 
-export default function InputLabel({ label = '', name = 'name', value = '', placeholder = '', type = 'text' }){
-    const types = ['text', 'number', 'date', 'time'];
+export default function InputLabel({ label = '', name = 'name', placeholder = '', type = 'text', options = [] }) {
+    const types = ['text', 'number', 'date', 'time', 'select'];
 
-    if(!types.includes(type)){
+    if (!types.includes(type)) {
         console.error('type is wrong to input form')
         return;
     }
 
+    if (type == 'select') {
+        return (
+            <div className='input__wrapper mb-3'>
+                <label>{label}</label>
+                <select name={name} placeholder={placeholder}>
+                    {
+                        options.map((value, index) => (
+                            <option value={value} key={index}>{value}</option>
+                        ))
+                    }
+                </select>
+            </div>
+        )
+    }
+
     return (
         <div className='input__wrapper mb-3'>
-            <label>{ label }</label>
-            <input name={name} value={value} placeholder={placeholder} type='text' />
+            <label htmlFor="age">{label}</label>
+            <input name={name} placeholder={placeholder} type='text' />
         </div>
     )
 }
--- src/pages/track-calori/partials/diagram-box/diagram-box.jsx
+++ src/pages/track-calori/partials/diagram-box/diagram-box.jsx
@@ -1,32 +1,32 @@
 import { Col, Row } from 'react-bootstrap';
 import './diagram-box.css';
-import CompositionDiagram from '../../component/composition-diagram/composition-diagram';
+// import CompositionDiagram from '../../component/composition-diagram/composition-diagram';
 
-export default function DiagramBox(){
+export default function DiagramBox({ caloriesNeed, caloriesAmount }) {
 
-    const composition = [
-        {
-            name: 'Karbohidrat',
-            count: 0,
-        },
-        {
-            name: 'Protein',
-            count: 0,
-        },
-        {
-            name: 'Lemak',
-            count: 0,
-        },
-        {
-            name: 'Karbondioksida',
-            count: 0,
-        }
-    ]
+    // const composition = [
+    //     {
+    //         name: 'Karbohidrat',
+    //         count: 0,
+    //     },
+    //     {
+    //         name: 'Protein',
+    //         count: 0,
+    //     },
+    //     {
+    //         name: 'Lemak',
+    //         count: 0,
+    //     },
+    //     {
+    //         name: 'Karbondioksida',
+    //         count: 0,
+    //     }
+    // ]
 
     return (
         <div className='diagram-box__wrapper'>
             <Row className='justify-content-between'>
-                <Col md={7}>
+                <Col md={12}>
                     <span className='required-calori_title'>Kebutuhan Kalori Harian</span>
                     <Row className='justify-content-between'>
                         <Col md={7}>
@@ -38,19 +38,19 @@ export default function DiagramBox(){
                             <ul className='calculate-calori'>
                                 <li>
                                     Dibutuhkan <br />
-                                    <span><strong>2070.99</strong> Kkal</span> 
+                                    <span><strong>{caloriesNeed}</strong> Kkal</span>
                                 </li>
                                 <li>
                                     Terpenuhi <br />
-                                    <span><strong>0</strong> Kkal</span> 
+                                    <span><strong>{caloriesAmount()}</strong> Kkal</span>
                                 </li>
                             </ul>
                         </Col>
                     </Row>
                 </Col>
-                <Col md={3}>
-                    {composition.map((item, index) =>  <CompositionDiagram key={index} name={item.name} count={item.count} />)}
-                </Col>
+                {/* <Col md={3}>
+                    {composition.map((item, index) => <CompositionDiagram key={index} name={item.name} count={item.count} />)}
+                </Col> */}
             </Row>
         </div>
     )
